7f657ff73a5d1851e846a3b6adca24bb1c0b4908
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-06-02 21:27:41 -0700

    Create the main frame used for the loot sheet

--- AxiomLootSheet.lua
+++ AxiomLootSheet.lua
@@ -1,14 +1,36 @@
-function HelloWorld(self) 
-	print("Hello World!"); 
-end
-
--- Slash Commands
-SLASH_AxiomLootSheet1 = "/axiom";
- 
+-------------------------------------------
+-- Slash Commands --
+-------------------------------------------
+SLASH_AxiomLootSheet1 = "/axiom"
 local function LootSheet(msg)
-	print("Axiom is a Horde guild on MoonGuard-US."); 
+	print("Axiom is a Horde guild on MoonGuard-US.")
 end
- 
-SlashCmdList["AxiomLootSheet"] = LootSheet;
+SlashCmdList["AxiomLootSheet"] = LootSheet
 
-----------------------------------------------------------------
+
+-------------------------------------------
+-- Functions --
+-------------------------------------------
+function HelloWorld(self)
+	print("Hello World!")
+end
+
+-------------------------------------------
+-- Frames --
+-------------------------------------------
+CreateFrame("Frame", "AxiomLootSheetFrame", UIParent, "BasicFrameTemplateWithInset")
+AxiomLootSheetFrame:SetSize(800, 600)
+AxiomLootSheetFrame:EnableMouse(true)
+AxiomLootSheetFrame:SetMovable(true)
+AxiomLootSheetFrame:SetClampedToScreen(true)
+AxiomLootSheetFrame:SetPoint("CENTER")
+AxiomLootSheetFrame:RegisterForDrag("LeftButton")
+AxiomLootSheetFrame:SetScript("OnDragStart", AxiomLootSheetFrame.StartMoving)
+AxiomLootSheetFrame:SetScript("OnDragStop", AxiomLootSheetFrame.StopMovingOrSizing)
+
+
+AxiomLootSheetFrame.title = AxiomLootSheetFrame:CreateFontString(nil, "OVERLAY")
+AxiomLootSheetFrame.title:SetFontObject("GameFontHighlightLarge")
+AxiomLootSheetFrame.title:SetPoint("LEFT", AxiomLootSheetFrame.TitleBg, "LEFT", 5, 0)
+AxiomLootSheetFrame.title:SetText("Axiom Loot Sheet")
+
