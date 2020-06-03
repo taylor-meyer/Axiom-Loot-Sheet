66dcc993e7a64462d97c90e9bb13c7c40652198e
Taylor Meyer <taylor.ross.meyer@gmail.com>
2020-06-02 19:11:05 -0700

    Add slash command /axiom

--- AxiomLootSheet.lua
+++ AxiomLootSheet.lua
@@ -1,3 +1,14 @@
 function HelloWorld(self) 
 	print("Hello World!"); 
 end
+
+-- Slash Commands
+SLASH_AxiomLootSheet1 = "/axiom";
+ 
+local function LootSheet(msg)
+	print("Axiom is a Horde guild on MoonGuard-US."); 
+end
+ 
+SlashCmdList["AxiomLootSheet"] = LootSheet;
+
+----------------------------------------------------------------
